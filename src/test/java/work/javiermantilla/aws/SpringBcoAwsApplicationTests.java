20e1dac957f565a12d0f41eed366ae4848054008
Javier Orlando Mantilla Portilla <jmantilla@gmail.com>
2024-10-29 08:19:23 -0500

    Se actualiza

--- src/test/java/work/javiermantilla/aws/SpringBcoAwsApplicationTests.java
+++ src/test/java/work/javiermantilla/aws/SpringBcoAwsApplicationTests.java
@@ -1,13 +1,13 @@
 package work.javiermantilla.aws;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBcoAwsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
